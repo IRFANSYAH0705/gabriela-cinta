068197f2d6e7ece219adb06e8e6aea96a415a401
IRFANSYAH0705 <muhammadirfansyah0404@gmail.com>
2026-09-19 12:25:50 +0700

    Add functions to open and close letter box

---
+++ script.js
@@ -0,0 +1,7 @@
+function openLetter() {
+    document.getElementById("letterBox").classList.add("show");
+}
+
+function closeLetter() {
+    document.getElementById("letterBox").classList.remove("show");
+}
